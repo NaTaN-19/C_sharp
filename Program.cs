48bc9d636b9b822a8123913ab18380e06f65cb0d
NaTaN-19 <52347201+NaTaN-19@users.noreply.github.com>
2026-06-10 23:05:06 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Задание 3
+//Пользователь вводит с клавиатуры четыре цифры. Необходимо создать число, содержащее эти цифры.
+
+//Например, если с клавиатуры введено 1, 5, 7, 8 тогда нужно сформировать число 1578. (С помощью арифметических операторов)
+
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+int num4 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(num1 * 1000 + num2 * 100 + num3 * 10 + num4 * 1);
