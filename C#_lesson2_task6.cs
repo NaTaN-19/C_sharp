8db4c9676a8a282d3cdf00e8577878ec4b5cec83
NaTaN-19 <52347201+NaTaN-19@users.noreply.github.com>
2026-06-11 20:16:47 +0400

    Add files via upload

---
+++ C#_lesson2_task6.cs
@@ -0,0 +1,30 @@
+﻿//Задание 6
+//Пользователь вводит с клавиатуры два числа.     
+//Нужно показать все четные числа в указанном диапазоне.
+//Если границы диапазона указаны неправильно требуется произвести нормализацию границ.
+//Например, пользователь ввел 20 и 11, требуется нормализация, после которой начало диапазона станет равно 11, а конец 20.
+
+
+Console.Write("Enter first number: ");
+int num1 = int.Parse(Console.ReadLine());
+
+
+Console.Write("Enter second number: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+if  (num1 > num2)
+{
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+}
+
+
+for (int i = num1; i <= num2; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine($"Even number from range is: {i}");
+    }
+}
