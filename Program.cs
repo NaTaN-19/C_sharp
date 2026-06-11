86b312db347c49b676c86a9ea0c192cef4c6c446
NaTaN-19 <52347201+NaTaN-19@users.noreply.github.com>
2026-06-11 20:37:52 +0400

    Delete Program.cs

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿//Задание 2
-//Пользователь вводит с клавиатуры два числа. Первое число — это значение, 
-//второе число процент, который необходимо посчитать. Например, мы ввели с клавиатуры 90 и 10. 
-//Требуется вывести на экран 10 процентов от 90. Результат: 9.
-
-double value = double.Parse(Console.ReadLine());
-double percentage = double.Parse(Console.ReadLine());
-Console.WriteLine(value * percentage/100);
